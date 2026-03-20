e94d90d1e82185a3c220d318473425482e68047c
Gabriel Florenino <gabriel.inacio@ufpe.br>
2026-03-20 20:01:39 -0300

    Hello.tcl added

---
+++ hello.tcl
@@ -0,0 +1,7 @@
+puts "Ola, munda - Entre aspas" ;# Comentario pos comando
+puts {Ola, mundo - Entre chaves}
+puts {Exemplo de sintaxe de comentario invalida}
+puts "Esta e a linha 1"; puts "esta e a linha 2"
+puts "Ola, mundo; - Com um ponto e virgula dentro das aspas"
+# Palavras nao precisam ser coladas entre aspas, a menos que contenham espacos
+puts Olamundo
