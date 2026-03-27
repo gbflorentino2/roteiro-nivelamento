d6271bf7f84ee693d86a5a4131a3fd03385dd7e6
andersonalexsandro <anderson.silva.04@outlook.com>
2026-03-27 20:46:38 -0300

    shell aluno b

--- shell_test/organizador.sh
+++ shell_test/organizador.sh
@@ -1,6 +1,44 @@
 #!/bin/bash
 
+diretorios=("src" "tb" "include" "scripts" "docs")
+
+for diretorio in  "${diretorios[@]}"; do
+
+	if [ ! -d $diretorio ]; then
+		mkdir $diretorio
+		echo "Pasta criada: " $diretorio
+	else
+		echo "Pasta  $diretorio já existe"
+	fi
+done
+
+
+
 for file in ./*
 do
-	echo "$file"
+	filepath=$file
+	filename_with_ext=$(basename "$filepath")
+
+	filename="${filename_with_ext%.*}"
+	extension="${filename_with_ext##*.}"
+
+	filename="${filename_with_ext%.*}"
+	extension="${filename_with_ext##*.}"
+
+	echo "============"
+	echo "Path: $filepath"
+	echo "Filename with extension: $filename_with_ext"
+	echo "Filename without extension: $filename"
+	echo "File extension: $extension"
+
+	if [ $extension == ".md" ]; then
+		if [ ! -f $filename_with_ext ]; then
+			mv $filepath ./docs/
+		else
+			echo "Arquivo $filename_with_ext Existe no diretório docs"
+		fi
+	fi 
 done
+
+
+
