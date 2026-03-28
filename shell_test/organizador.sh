3f34682f661e175587cedb61ac4d687576fa115e
andersonalexsandro <anderson.silva.04@outlook.com>
2026-03-28 01:04:56 -0300

    organizador de arquivos feito

--- shell_test/organizador.sh
+++ shell_test/organizador.sh
@@ -18,26 +18,49 @@ for file in ./*
 do
 	filepath=$file
 	filename_with_ext=$(basename "$filepath")
-
 	filename="${filename_with_ext%.*}"
 	extension="${filename_with_ext##*.}"
 
-	filename="${filename_with_ext%.*}"
-	extension="${filename_with_ext##*.}"
 
-	echo "============"
-	echo "Path: $filepath"
-	echo "Filename with extension: $filename_with_ext"
-	echo "Filename without extension: $filename"
-	echo "File extension: $extension"
-
-	if [ $extension == ".md" ]; then
-		if [ ! -f $filename_with_ext ]; then
-			mv $filepath ./docs/
+	if [[ $extension == "md" || $extension == "txt" ]]; then
+		if [ ! -f ./docs/$filename_with_ext ]; then
+			mv $file ./docs/
 		else
 			echo "Arquivo $filename_with_ext Existe no diretório docs"
 		fi
 	fi 
+
+	if	[[ $extension == "tcl" || $extension == "do" || $extension == "sh" ]]; then
+		if [ ! -f ./scripts/$filename_with_ext ]; then
+			mv $file ./scripts/
+		else
+			echo "Arquivo $filename_with_ext Existe no diretório scripts"
+		fi
+	fi
+
+	if [ $extension = "vh" ]; then
+		if [ ! -f ./include/$filename_with_ext ]; then
+			mv $file ./include/
+		else
+			echo "Arquvio $filename_with_ext Existe no diretório include"
+		fi
+	fi
+
+	if [[$extension == "v" ]]; then
+		if [[$filename == *"_tb"*]]; then
+			if [ ! -f ./tb/$filename_with_ext ]; then
+				mv $file ./tb
+			else 
+				echo "Arquvio $filename_with_ext Existe no diretório tb"
+			fi
+		else
+			if [ ! -f ./src/$filename_with_ext ]; then
+				mv $file ./src
+			else
+				echo "Arquvio $filename_with_ext Existe no diretório src"
+			fi
+		fi
+	fi
 done
 
 
